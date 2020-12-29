384d20ad4a19c22076cc1531870e109f2d9484da
Marco Guidi <mark.88g@gmail.com>
2020-12-29 15:02:13 +0100

    layout responsive header 1/2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <title>@yield('title', 'Una parola al giorno')</title>
+
+        <!-- css -->
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
         @include('partials.header')
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,86 @@
+<header>
+    <!-- top: logo and user access -->
+    <div id="header-logo">
+        <div class="container">
+            <div class="row">
+                <!-- sx: brand logo -->
+                <div class="logo">
+                    <img src="https://unaparolaalgiorno.it/_nuxt/img/Logo.0d34260.png" alt="brand logo">
+                </div>
+
+                <!-- dx: user access -->
+                <div class="user-access">
+                    ciao
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- hidden-btm: nav menu -->
+    <nav id="nav-menu">
+        <div class="container">
+            <div class="row">
+                <!-- sx: menu -->
+                <div class="main-menu">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                progetto
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                manifesto
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                sfoglia
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                cicli di parole
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                libri
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                articoli
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                contatti
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                collabora
+                            </a>
+                        </li>
+                        <li class="btn-sec">
+                            <a href="#">
+                                premium
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                bottega
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- dx: search -->
+                <div class="search">
+
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
