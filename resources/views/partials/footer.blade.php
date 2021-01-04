35055466696f8b16766b140c62290c1da0127f43
Marco Guidi <mark.88g@gmail.com>
2021-01-04 12:22:54 +0100

    layout footer e stile base

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,63 @@
+<footer>
+    <!-- top: footer menu -->
+    <div id="footer-menu">
+        <div class="container">
+            <div class="row">
+                <ul class="footer-menu">
+                    <li class="footer-submenu">
+                        <span class="submenu-title">UPAG</span>
+                        <ul class="submenu">
+                            <li>Il progetto</li>
+                            <li>Manifesto</li>
+                            <li>Chi siamo</li>
+                            <li>FAQ - Domande e risposte</li>
+                            <li>Articoli</li>
+                        </ul>
+                    </li>
+                    <li class="footer-submenu">
+                        <span class="submenu-title">Partecipa</span>
+                        <ul class="submenu">
+                            <li>Contatti/Proponi</li>
+                            <li>Collabora</li>
+                            <li>Quiz</li>
+                            <li>Mappa delle parole</li>
+                        </ul>
+                    </li>
+                    <li class="footer-submenu">
+                        <span class="submenu-title">Seguici</span>
+                        <ul class="submenu">
+                            <li>Iscriviti</li>
+                            <li>Facebook</li>
+                            <li>Instagram</li>
+                            <li>Alexa</li>
+                        </ul>
+                    </li>
+                    <li class="footer-submenu">
+                        <span class="submenu-title">Note legali</span>
+                        <ul class="submenu">
+                            <li>Condizioni - Termini di servizio</li>
+                            <li>Cookie policy</li>
+                            <li>Privacy policy</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- end: container -->
+        </div><!-- end: container -->
+    </div><!-- end: #footer-menu -->
+
+    <!-- btm: footer copy -->
+    <div id="footer-copy">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 copy-info">
+                    <p>
+                        I contenuti di questo sito sono distribuiti con <a href="#">Licenza Creative Commons Attribuzione - Non commerciale - Condividi allo stesso modo 4.0</a>.
+                    </p>
+                    <p>
+                        UPAG SRLS - Via Europa 199, Vaglia - P.IVA 06890420489 - REA: FI-664147 - ISSN 2704-727X
+                    </p>
+                </div>
+            </div><!-- end: container -->
+        </div><!-- end: container -->
+    </div><!-- end: #footer-copy -->
+</footer>
